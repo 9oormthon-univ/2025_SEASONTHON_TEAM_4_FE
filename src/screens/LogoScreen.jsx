86fd3ee627084417f76629db83871d61fa638acf
yerin-lab <chayerin_@naver.com>
2025-09-06 23:55:30 +0900

    온보딩 1

--- src/screens/LogoScreen.jsx
+++ src/screens/LogoScreen.jsx
@@ -4,6 +4,7 @@ import "./LogoScreen.css";
 import Signup from "./Signup.jsx";
 import Login from "./Login.jsx";
 import logo from "../assets/logo.png"
+import kakao from "../assets/kakao.png"
 
 function LogoScreen () {
     const navigate = useNavigate();
@@ -18,33 +19,44 @@ function LogoScreen () {
 
     return (
         <div className={`screen ${step === "first" ? "screen-1" : "screen-2"}`}>
-            <div
-                className={`logo ${step === "second" ? "logo-up" : ""}`}
-            >
-                <img src={logo} alt="logo" className="logo-image" />
+            <div className="top-content">
+                <div className={`logo ${step === "second" ? "logo-up" : ""}`}>
+                    <img src={logo} alt="logo" className="logo-image" />
+                </div>
+                {step === "second" && (
+                    <>
+                        <h1 className="app-name">단짝</h1>
+                        <h2 className="app-content">1형 당뇨인을 위한 AI 비서</h2>
+                    </>
+                )}
             </div>
 
             {step === "second" && (
-                <div className="content">
-                    <h2 className="app-name">Glucare</h2>
-                    <div className="button-container">
-                        <button
-                            className="action-button"
-                            onClick={() => navigate("/Signup")}
-                        >
-                            회원가입
-                        </button>
-                        <button
-                            className="action-button"
-                            onClick={() => navigate("/Login")}
-                        >
-                            로그인
-                        </button>
-                    </div>
+                <div className="bottom-buttons">
+                    <button
+                        className="action-button kakao-login"
+                        onClick={() => navigate("/Signup")}
+                    >
+                        <img src={kakao} alt="카카오 아이콘" className="kakao-icon" />
+                        카카오 로그인
+                    </button>
+                    <button
+                        className="action-button signup-button"
+                        onClick={() => navigate("/Signup")}
+                    >
+                        회원가입
+                    </button>
+                    <button
+                        className="action-button code-login"
+                        onClick={() => navigate("/Signup-with-code")}
+                    >
+                        <span className="highlight">아이 코드</span>로 로그인하기
+                    </button>
                 </div>
             )}
         </div>
     );
+
 }
 
 export default LogoScreen
