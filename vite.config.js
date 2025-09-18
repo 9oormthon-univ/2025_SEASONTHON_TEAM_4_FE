62f031e42bbe0b64e5dfcf12084a86197d120428
philosophia301 <eunjong147@gmail.com>
2025-09-18 16:41:26 +0900

    feat: PWA 적용

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    VitePWA({
+      manifest: {
+        name: '단짝이',
+        short_name: '단짝이',
+        theme_color: '#00BBA9',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/src/assets/icon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   server: {
     proxy: {
       '/api': {
